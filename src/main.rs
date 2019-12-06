2dc7ad560ce4c81e2482bbf97c3051f4776035a0
Luke Titley <luke@titley.com>
2019-12-06 23:42:11 +0100

    Recursive iteration is working. Now need to put it into a thread

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,40 @@
-use std::path;
 use std::env;
-use std::result::Result;
+use std::fs;
+use std::io;
+use std::path;
+use std::vec;
 
-type Error = Result<(), std::io::Error>;
+type Paths = vec::Vec<path::PathBuf>;
+
+type Error = io::Result<()>;
 
 fn list_repos() -> Error {
-    let current_dir = env::current_dir()?.as_path();
+    let current_dir = env::current_dir()?;
+
+    let mut paths = Paths::new();
+
+    paths.push(current_dir);
+
+    // Walk over the directory
+    while !paths.is_empty() {
+        let path = paths.pop().unwrap();
+        for entry in fs::read_dir(path)? {
+            let p = entry?.path();
+            if p.is_dir() {
+                let mut p_buf = p.to_path_buf();
+                let name = p.file_name().unwrap().to_str();
+                match name {
+                    Some(".git") => {
+                        p_buf.pop();
+                        println!("{0}", p_buf.as_path().display());
+                    }
+                    _ => {
+                        paths.push(p_buf);
+                    }
+                }
+            }
+        }
+    }
 
     Ok(())
 }
