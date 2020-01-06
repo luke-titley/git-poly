1f9e1471fbbc001238d7ee83dcda9c39cd98ccf0
Luke Titley <luke@titley.com>
2020-01-06 13:28:19 +0100

    Put platform configurations into a folder called platforms

--- build_platform.sh
+++ build_platform.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 PLATFORM=$1
+TEMPNAME=cont1_${RANDOM}
 
-mkdir -p ${PLATFORM}/target
-rm -rf ${PLATFORM}/target
+mkdir -p platforms/${PLATFORM}/target
+rm -rf platforms/${PLATFORM}/target
 
 # Do the build
-docker build -t git-poly:${PLATFORM} -f ${PLATFORM}/Dockerfile .
-docker create --name cont1 git-poly:${PLATFORM}
-docker cp cont1:/target ${PLATFORM}/target
-docker rm cont1
+docker build -t git-poly:${PLATFORM} -f platforms/${PLATFORM}/Dockerfile .
+docker create --name ${TEMPNAME} git-poly:${PLATFORM}
+docker cp ${TEMPNAME}:/target platforms/${PLATFORM}/target
+docker rm ${TEMPNAME}
